6e87e6ea3efc9e8f5337f75d3ecf4679ab9f622c
JSPJAVA1203 <ray589@naver.com>
2023-10-24 14:23:52 +0900

    class Content, Title 써보기 + JSX 주석 넣는 방법

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { Component } from "react";
+// Component : 가독성 / 재사용성 / 유지 보수성을 높이기 위해 복잡한 HTML 코드를 모듈화 할 수 있는 최소 단위
+class Content extends Component{
+  render(){
+    return(
+      <div>이건 Content 입니다.</div>
+    );
+  }
+}
 
+class Title extends Component{
+  render(){
+    return(
+      <div> 이건 Title 입니다.</div>
+    );
+  }
+}
 class App extends Component{
   render(){
     return (
       <div>
-        Hello React!
+        <Title/> {/* 아니 여기엔 주석 어케씀?? */}
+        <Content/>
       </div>
     );
   }
