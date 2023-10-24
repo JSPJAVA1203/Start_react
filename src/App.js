edc2ecf09d27ca55a49d799b905ef65ec726411d
JSPJAVA1203 <ray589@naver.com>
2023-10-24 13:59:18 +0900

    App.js 주석 추가

--- src/App.js
+++ src/App.js
@@ -9,4 +9,7 @@ class App extends Component{
     );
   }
 }
+// App이라는 Component가 구현되어 있다. 
+// App은 class이며 Component를 상속받는다. App Component를 사용하면 <div>Hello World!</div> HTML 태그를 렌더링 하게 된다.
+
 export default App;
